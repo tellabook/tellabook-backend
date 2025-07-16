fadc3f9cf6beb7920eb2f2e80f791edcb2570c79
tellabook <kobykano@gmail.com>
2025-07-16 08:40:11 -0700

    Fix host/port for Render deployment

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from app.routes import bp
+import os
 
 app = Flask(__name__)
 app.register_blueprint(bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
