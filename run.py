680611f285f21f6c0692b041f7e76209117d4d29
tellabook <kobykano@gmail.com>
2025-07-18 14:02:03 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,18 @@
-import os
 from flask import Flask
-from app.models import init_app as db_init_app
+from app.models import db
 from app.routes import bp
+import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# DB config (used by init_app inside models)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-# Initialize database
-db_init_app(app)
-
-# Register blueprint
+db.init_app(app)
 app.register_blueprint(bp, url_prefix="/")
 
 @app.route("/")
 def index():
     return "Tellabook backend is running!"
 
-# Make sure it binds to external port
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host="0.0.0.0", port=port)
+    app.run(debug=True)
